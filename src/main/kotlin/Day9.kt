d454b9b1aaeca46ebf6ed52cfe2ac50410010594
Noah Colvin <7552226+noahcolvin@users.noreply.github.com>
2022-12-10 09:13:49 -0500

    Day9 part 2, finally

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -71,22 +71,91 @@ class Day9 {
                         }
                     }
                 }
-
             }
         }
-
-        println(tailMoves)
 
         return tailMoves.count()
     }
 
+    private fun whip(knots: List<Knot>, tailMoves: MutableList<String>) {
+        var last: Knot? = null
+
+        for (knot in knots) {
+            if (last == null) {
+                last = knot
+                continue
+            }
+
+            if (knot.column == last.column && knot.row + 1 < last.row) {
+                knot.row++
+                if (knots.last() == knot) tailMoved(tailMoves, knot)
+            } else if (knot.column != last.column && knot.row + 1 < last.row) {
+                knot.row++
+                if (last.column > knot.column) knot.column++ else knot.column--
+                if (knots.last() == knot) tailMoved(tailMoves, knot)
+            } else if (knot.column == last.column && knot.row - 1 > last.row) {
+                knot.row--
+                if (knots.last() == knot) tailMoved(tailMoves, knot)
+            } else if (knot.column != last.column && knot.row - 1 > last.row) {
+                knot.row--
+                if (last.column > knot.column) knot.column++ else knot.column--
+                if (knots.last() == knot) tailMoved(tailMoves, knot)
+            } else if (knot.row == last.row && knot.column + 1 < last.column) {
+                knot.column++
+                if (knots.last() == knot) tailMoved(tailMoves, knot)
+            } else if (knot.row != last.row && knot.column + 1 < last.column) {
+                knot.column++
+                if (last.row > knot.row) knot.row++ else knot.row--
+                if (knots.last() == knot) tailMoved(tailMoves, knot)
+            } else if (knot.row == last.row && knot.column - 1 > last.column) {
+                knot.column--
+                if (knots.last() == knot) tailMoved(tailMoves, knot)
+            } else if (knot.row != last.row && knot.column - 1 > last.column) {
+                knot.column--
+                if (last.row > knot.row) knot.row++ else knot.row--
+                if (knots.last() == knot) tailMoved(tailMoves, knot)
+            }
+
+            last = knot
+        }
+    }
+
     private fun tailMoved(tailMoves: MutableList<String>, tail: Knot) {
-        val move = "${tail.row},${tail.column}"
+        val move = "(${tail.column}, ${tail.row})"
         if (!tailMoves.contains(move))
             tailMoves.add(move)
     }
 
     fun part2(data: List<String>): Int {
-        return 0
+        val knots = listOf(Knot(), Knot(), Knot(), Knot(), Knot(), Knot(), Knot(), Knot(), Knot(), Knot())
+        val tailMoves = mutableListOf<String>()
+        tailMoved(tailMoves, knots.last())
+
+        for (line in data) {
+            val move = line.split(" ")
+            for (x in 1..move[1].toInt()) {
+                when (move[0]) {
+                    "U" -> {
+                        knots.first().row++
+                    }
+
+                    "D" -> {
+                        knots.first().row--
+                    }
+
+                    "R" -> {
+                        knots.first().column++
+                    }
+
+                    "L" -> {
+                        knots.first().column--
+                    }
+                }
+
+                whip(knots, tailMoves)
+            }
+        }
+
+        return tailMoves.count()
     }
 }
